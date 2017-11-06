faa25150a7c9f05655f290bf85f74e85414484d7
Hendrik Schawe <surt91@gmail.com>
2017-11-06 20:29:33 +0100

    fill the spaces with the dominant color before the preview is shown

--- preload_thumbnail/preload_thumbnail.py
+++ preload_thumbnail/preload_thumbnail.py
@@ -4,7 +4,7 @@
 # </div>
 
 import os
-from subprocess import call
+from subprocess import call, check_output
 import logging
 import base64
 
@@ -58,8 +58,10 @@ def replace_images(f, settings):
 
         create_thumbnail(image_local, thumbnail_local)
         data_url = "data:image/jpeg;base64," + base64.b64encode(open(thumbnail_local, "rb").read()).decode("utf-8")
+        color = get_dominant_color(thumbnail_local)
+        div_tag["style"] = "background-color: " + color
 
-        logger.info("Convert {} -> {}B preview".format(basename, len(data_url)))
+        logger.info("Convert {} -> {}B preview ({})".format(basename, len(data_url), color))
 
         new_img = soup.new_tag("img")
         new_img["src"] = data_url
@@ -82,6 +84,17 @@ def create_thumbnail(infile, outfile):
             os.path.getmtime(infile) > os.path.getmtime(outfile):
         print(" ".join(cmd))
         call(cmd)
+
+
+def get_dominant_color(image_file):
+    out = check_output(["convert", image_file, "+dither", "-colors", "5", "-define", "histogram:unique-colors=true", "-format", "%c", "histogram:info:"]).decode("utf-8")
+    lines = out.split("\n")
+    line = sorted(lines)[-1]
+    rgb = line.strip().split("#")[1]
+    r = rgb[0:2]
+    g = rgb[2:4]
+    b = rgb[4:6]
+    return "#{}{}{}".format(r, g, b)
 
 
 def register():
